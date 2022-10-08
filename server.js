9ce5e53074c8bd04a1bd3046fd29143698d67bb3
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-08 13:51:04 +0300

    basic app configuration

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express')
+const hbs = require('express-handlebars').create({
+    extname: '.hbs'
+});
+const port = 8000;
+const app = express();
+
+app.engine('.hbs', hbs.engine)
+app.set('view engine', '.hbs')
+
+//global middlewares
+app.use(express.urlencoded({extended:false}))
+app.use('/static',express.static('static'))
+
+app.listen(port,()=>console.log(`App listening on port ${port}...`))
