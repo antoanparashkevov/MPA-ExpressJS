75a0af525427a17969f6d83bbf07beab05407272
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-16 08:34:04 +0300

    basic validation for missing fields

--- services/roomService.js
+++ services/roomService.js
@@ -8,15 +8,24 @@ const getById =  (id)=>{
     //lean extracts only the values, not methods or virtual properties just for security (express-handlebars error)
     return Room.findById(id).lean()
 }
-const create = async (data) => {
-    return await Room.create({
+const create =  (data) => {
+    const room = {
         name: data.name,
         description: data.desc,
         location: data.location,
         price: data.price,
         beds: data.beds,
         imgURL: data.img
-    });
+    }
+    
+    const missing = Object.entries(room).filter(([k,v])=> !v);
+    
+    if(missing.length > 0) {
+        throw new Error(missing.map(m=>`${m[0]} is required!`).join('\n'));
+    }
+
+
+    return Room.create(room);
 }
 
 module.exports = {
