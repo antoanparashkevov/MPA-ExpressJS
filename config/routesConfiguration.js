8dcad6e6af25967e4a1e014468661ba20b4eade8
“antoanparashkevov” <tonkata1505@gmail.com>
2022-10-28 22:31:29 +0300

    Removed unnecessary middleware guards

--- config/routesConfiguration.js
+++ config/routesConfiguration.js
@@ -7,16 +7,16 @@ const defaultController = require('../controllers/defaultController')
 const authController = require('../controllers/authController')
 const editController = require('../controllers/editController')
 const deleteController = require('../controllers/deleteController')
-const {isUser, isGuest} = require("../middlewares/guards");
+const {isUser} = require("../middlewares/guards");
 
 module.exports = (app) =>{
     app.use(homeController);
     app.use('/catalog',catalogController);
     app.use('/create',isUser(), createController);
-    app.use('/room', isUser(), editController)
-    app.use('/room', isUser(), deleteController)
+    app.use('/room' , editController)
+    app.use('/room', deleteController)
     app.use('/about',aboutController);
-    app.use('/facility', isUser(),facilityController);
-    app.use('/auth',isGuest(), authController)
+    app.use('/facility',facilityController);
+    app.use('/auth', authController)
     app.all('/*',defaultController)
 }
