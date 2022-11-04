7503b3a97ccbd2e1f9622557e8c88c2406fe5464
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-04 21:28:32 +0200

    Validate the image url using the Mongoose validator

--- models/Room.js
+++ models/Room.js
@@ -1,5 +1,7 @@
 const {Schema, model, Types} = require('mongoose')
 
+const URL_REGEX = /^https?:\/\/.*$/i
+
 const roomSchema = new Schema({
 
     name: {type: String, required: true},
@@ -7,7 +9,16 @@ const roomSchema = new Schema({
     location: {type: String, required: true},
     beds:{type: Number, min: [1, 'Must supply at least 1 bed to your accommodation'], required: true},
     price: {type: Number, min: [0.01, 'Price must be a positive number'], required: true},
-    imgURL: {type: String},
+    imgURL: {
+        type: String,
+        validate: {
+            validator: (value) => URL_REGEX.test(value),
+            message: (props) =>{
+                console.log('All props from validation process (ImgURL)', props)
+                return `${props.value} is not a valid Image Url`
+            }
+        }
+    },
     facilities:{type: [Types.ObjectId], default: [], ref: 'Facility'},
     owner: {type: Types.ObjectId, ref: 'User', required: true}
 })
