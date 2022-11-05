4ca6d0e045b4c9a1a75da4973876a82781573133
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-05 08:57:38 +0200

    Form for edit room process error validation

--- controllers/editController.js
+++ controllers/editController.js
@@ -1,4 +1,5 @@
 const {getById, update, create} = require("../services/roomService");
+const {parseError} = require("../util/parser");
 const router = require('express').Router()
 
 
@@ -19,27 +20,21 @@ router.get('/:id/edit', async (req, res) => {
 router.post('/:id/edit', async (req,res) => {
     const roomId = req.params.id;
     const roomData = req.body;
-    console.log('formData', roomData)
+    
     const room = await getById(roomId)
-    console.log('room',room)
     if(!req.user || (room.owner.toString() !== req.user._id)) {
         return res.redirect('/auth/login')
     }
     
     try{
-         await update(roomId, roomData)
+        await update(roomId, roomData)
         res.redirect('/catalog/' + roomId)
     }catch (err){
-        //TODO error conditional statement in the view.
         req.body._id = roomId
-        console.log(err.message.split('\n'))
         res.render('pages/edit',{
-            title: 'Update error',
-            error: err.message.split('\n'),
-            room: {
-                ...req.body,
-                imgURL: req.body.imgUrl
-            }
+            title: 'Update page',
+            error:  parseError(err),
+            room: req.body
         })
     }
     
