4f725cc055d2c262836361fda671fae9fe260513
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-05 09:05:49 +0200

    If there is no data available, to display something

--- controllers/createController.js
+++ controllers/createController.js
@@ -4,12 +4,12 @@ const {create} = require('../services/roomService')
 const {hasRole} = require("../middlewares/guards");
 const {parseError} = require("../util/parser");
 
-router.get('/',hasRole('admin'), (req,res)=>{
+router.get('/', (req,res)=>{
     res.render('pages/create',{
         title:'Create page'
     })
 })
-router.post('/',hasRole('admin'), async (req,res)=>{
+router.post('/', async (req,res)=>{
     const formData = req.body;
     try{
         const result =  await create(formData, req.user._id)
