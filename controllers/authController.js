7e77a0fba63b56acc3f991e379d19717a443c340
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-05 08:41:43 +0200

    Login process error validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const {login, register} = require("../services/authService");
 const { body, validationResult } = require('express-validator');
+const {parseError} = require("../util/parser");
 
 router.get('/login', (req,res)=>{
     res.render('pages/login', {
@@ -8,11 +9,25 @@ router.get('/login', (req,res)=>{
     })
 })
 
-router.post('/login',async (req,res)=>{
+router.post('/login',
+    body(['username','password']).trim(),
+    async (req,res)=>{
     const formData = req.body;
-    const result = await login(formData.username, formData.password)//from the resolve function
-    attachToken (req,res,result)
-    res.redirect('/')
+    try {
+        const {errors} = validationResult(req)
+        if(errors.length > 0) {
+            throw errors;
+        }
+        const result = await login(formData.username, formData.password)//from the resolve function
+        attachToken (req,res,result)
+        res.redirect('/')
+    }catch (error) {
+        res.render('pages/login', {
+            title: 'Login page',
+            error: parseError(error),
+            username: req.body.username
+        })
+    }
 })
 
 router.get('/register', (req,res)=>{
@@ -33,7 +48,7 @@ router.post('/register',
     body('password')
         .trim()
         .isLength({min: 3})
-        .withMessage('Passowrd must be at least 3 characters long'),
+        .withMessage('Password must be at least 3 characters long'),
     body('repass')
         .trim()
         .custom(async (value, {req}) =>{
@@ -52,12 +67,10 @@ router.post('/register',
         attachToken(req, res, result)
         res.redirect('/')
     }catch (error) {
-        const fields = Object.fromEntries(error.map(e=>[e.param, e.param]))
-        fields.oldValueName = req.body.username;
         res.render('pages/register', {
             title: 'Register page',
-            error,
-            fields
+            error: parseError(error),
+            username: req.body.username
         })
     }
 })
